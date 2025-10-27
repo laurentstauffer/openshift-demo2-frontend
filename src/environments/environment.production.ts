2972a3756dc12d2ebe613e77d351a8e11d9093c1
laurentstauffer <laurentstauffer@gmail.com>
2025-10-27 16:48:00 +0100

    Fix: Angular appelle directement backend HTTPS (sans proxy nginx)

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: ''  // Vide car nginx ajoute déjà /api/ dans la configuration du proxy
+  apiUrl: 'https://openshift-demo-2-backend-git-laurentstauffer-dev.apps.rm3.7wse.p1.openshiftapps.com'
 };

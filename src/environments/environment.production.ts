44c7c97ced64443623cdaf658232f3e8fd08a0c4
laurentstauffer <laurentstauffer@gmail.com>
2025-10-27 18:35:13 +0100

    Fix CORS: Ajouter proxy nginx pour éviter les erreurs CORS

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://openshift-demo-2-backend-git-laurentstauffer-dev.apps.rm3.7wse.p1.openshiftapps.com'
+  apiUrl: ''  // Utiliser le proxy nginx local
 };
